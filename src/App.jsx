586ba0596574b8e7205b4ab50c6abca42c0b5282
Angel Serrato <serrato.anggel@gmail.com>
2026-04-17 08:51:48 -0500

    feat: implement product preview card structure and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import cartIcon from './assets/icon-cart.svg';
+import imageProduct from './assets/image-product-mobile.jpg';
+
 function App() {
   return (
-    <div>
-      <h1 className="font-extrabold underline">
-        Product Preview Card Component
-      </h1>
-    </div>
+    <main className="flex flex-col items-center justify-center">
+      <figure>
+        <img
+          src={imageProduct}
+          alt="Perfume bottle of Gabrielle Essence by Chanel Paris"
+        />
+      </figure>
+      <p>Perfume</p>
+      <h1>Gabrielle Essence Eau De Parfum</h1>
+      <p>
+        A floral, solar and voluptuous interpretation composed by Olivier Polge,
+        Perfumer-Creator for the House of CHANEL.
+      </p>
+      <p>$149.99</p>
+      <p>$169.99</p>
+      <button>
+        <img src={cartIcon} alt="Shopping cart icon" />
+        Add to Cart
+      </button>
+    </main>
   );
 }
 
